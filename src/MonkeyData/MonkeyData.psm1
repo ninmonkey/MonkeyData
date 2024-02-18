10289a7057f83e272457a154c899a109acdbaadf
Jake Bolton <ninmonkeys@gmail.com>
2024-02-18 13:09:13 -0600

    Expands more default properties

--- src/MonkeyData/MonkeyData.psm1
+++ src/MonkeyData/MonkeyData.psm1
@@ -51,7 +51,9 @@ function MonkeyData.SummarizeObjectType  {
         [string]$Kind,
 
         [Parameter(Mandatory, ValueFromPipeline)]
-        $InputObject
+        $InputObject,
+
+        [switch]$WithoutCalculatedProps
 
     )
     process {
@@ -62,19 +64,74 @@ function MonkeyData.SummarizeObjectType  {
                 [Microsoft.SqlServer.Management.Smo.Database]$Db = $InputObject
                 # $Db | Select-object -Property Query
 
-                $Obj | Select-Object -prop @(
-                    'ComputerName',
-                    'InstanceName',
-                    'SqlInstance',
+                $newObj = $Obj | Select-Object -ea 'ignore' -prop @(
+                    'Owner',
                     'Name',
+                    'SqlInstance',
+                    'ServerVersion',
+
+                    'SpaceAvailable'
+                    'CompatibilityLevel'
                     'Status',
                     'SizeMB',
+                    'Version'
+                    'UserName'
+                    'LogFiles',
+
+                    'PrimaryFilePath'
                     'Compatibility',
                     'QueryStoreOptions',
                     'Collation',
                     'Encrypted',
-                    'Last*Backup'
+                    'Last*Backup',
+                    '*Name*',
+                    '*Options*',
+                    'Tables',
+                    'StoredProcedures','UserDefined*',
+                    'Assemblies','ExternalLanguages','ExternalLibraries',
+                    'DatabaseScopedConfigurations'
+                    'UserDefinedFunctions'
+                    'ExtendedStoredProcedures'
+                    'Views', 'Users'
+                    'Schemas',
+                    'Partition*'
+                    'ServiceBroker'
+                    'ComputerName', # redundant with [SqlInstance]
+                    'InstanceName', # redundant with [SqlInstance]
+                    'Collation'
+                    'ReadOnly'
+                    'DefaultLanguage'
+                    'Size' # redundant With [SizeMB]
                 )
+                $newProps = [ordered]@{
+                    md_Func       = $Db | Select-Object -prop '*Functions*' -ea 'ignore'
+                    md_Procedures = $Db | Select-Object -prop '*StoredProcedures*' -ea 'ignore'
+                    md_Props      = $Db.Properties
+                    md_Tables     = $Db | Select-Object -prop '*Tables*' -ea 'ignore'
+                    md_Views      = $Db | Select-Object -prop '*Views*' -ea 'ignore'
+                    md_Is         = $DB | select-object -property 'Is*' -ea 'ignore'
+                    md_Names      = $DB | select-object -property '*Name*' -ea 'ignore'
+                    md_Versions   = $DB | select-object -property '*Version*' -ea 'ignore'
+                    md_Date       = $DB | select-object -property '*Date*' -ea 'ignore'
+
+                    Props         = $Db.Properties
+                }
+                $addMemberSplat = @{
+                    ErrorAction         = 'ignore'
+                    Force               = $true
+                    NotePropertyMembers = $newProps
+                    PassThru            = $true
+                    TypeName            = 'MonkeyData.Smo.Database.Summary'
+                }
+
+                if($WithoutCalculatedProps) {
+                    $newObj
+                    break
+                }
+
+                $NewObj | Add-Member @addMemberSplat
+                break
+
             }
             default { throw "UnhandledTypeName: $Kind"}
         }
--- src/debug_harness.ps1
+++ src/debug_harness.ps1
@@ -1,14 +1,16 @@
-﻿
-
+﻿# if($ScriptConf){ $ScriptConf.FirstLoad = $false }
+$script:ScriptConf ??= @{
+    FirstLoad = $true
+}
 # Invoke-DbaQuery -SqlInstance nin8\sql2019 -Database gLate -
 push-location $PSScriptRoot
 impo (Join-Path $PSScriptRoot './MonkeyData/MonkeyData.psd1') -PassThru -Force -ea 'stop'
 MonkeyData.GetDb|ft -AutoSize
-
 $db = MonkeyData.GetDb
-$db[0].QueryStoreOptions | % Properties|ft
-
-$db | MonkeyData.SummarizeObjType Smo.Database | ft
+# $db[0].QueryStoreOptions | % Properties|ft
+if($ScriptConf.FirstLoad) {
+    $SCriptConf.FirstLoad = $false
+    $db | MonkeyData.SummarizeObjType Smo.Database | ft
+}
 
 Dotils.QuickGcm.ByPrefix -Pattern 'MonkeyData.*'
-MonkeyData.GetDb | MonkeyData.SummarizeObj Smo.Database | ft
